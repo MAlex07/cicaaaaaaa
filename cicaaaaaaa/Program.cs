ee7301e43c9a67111147d3baabd970e622355aa8
MAlex07 <molnar.alex@petrik.hu>
2025-02-25 18:06:52 +0100

    Kész a példányosítás

--- cicaaaaaaa/Program.cs
+++ cicaaaaaaa/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Cicaaa c1 = new("Rozi", 9, 8, "házi macska", "cirmos", 15, 5, "nőstény");
+            Cicaaa c2 = new("Csizmás Kandúr", 3, 4, "házi macska", "narancssárga", 70, 4, "kandúr");
+            Cicaaa c3 = new("Cirmi", 4, 9, "Maine Coon", "fehér", 40, 7, "nőstény");
 
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
         }
     }
 }
